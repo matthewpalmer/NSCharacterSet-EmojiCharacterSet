fef03f513233c3db4e36359d3266f77f16009688
matthewpalmer <matt@matthewpalmer.net>
2016-05-07 16:39:54 +1000

    Initial Commit

---
+++ NSCharacterSet+EmojiCharacterSet/NSCharacterSet+EmojiCharacterSet.h
@@ -0,0 +1,19 @@
+//
+//  NSCharacterSet+EmojiCharacterSet.h
+//  NSCharacterSet+EmojiCharacterSet
+//
+//  Created by Matthew Palmer on 7/05/2016.
+//  Copyright © 2016 Matthew Palmer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NSCharacterSet+EmojiCharacterSet.
+FOUNDATION_EXPORT double NSCharacterSet_EmojiCharacterSetVersionNumber;
+
+//! Project version string for NSCharacterSet+EmojiCharacterSet.
+FOUNDATION_EXPORT const unsigned char NSCharacterSet_EmojiCharacterSetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NSCharacterSet_EmojiCharacterSet/PublicHeader.h>
+
+
---
+++ NSCharacterSet+EmojiCharacterSetTests/NSCharacterSet_EmojiCharacterSetTests.swift
@@ -0,0 +1,36 @@
+//
+//  NSCharacterSet_EmojiCharacterSetTests.swift
+//  NSCharacterSet+EmojiCharacterSetTests
+//
+//  Created by Matthew Palmer on 7/05/2016.
+//  Copyright © 2016 Matthew Palmer. All rights reserved.
+//
+
+import XCTest
+@testable import NSCharacterSet_EmojiCharacterSet
+
+class NSCharacterSet_EmojiCharacterSetTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
